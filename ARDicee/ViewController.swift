053c5470149ef8c395d443cfc37cc1300592c652
Adam Stanisławski <poncki1643@gmail.com>
2018-03-25 18:17:13 +0200

    small changes

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -19,53 +19,18 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Set the view's delegate
         sceneView.delegate = self
         
         self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
         
-        //let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
-        
-//        let sphere = SCNSphere(radius: 0.2)
-//
-//        let material = SCNMaterial()
-//
-//        material.diffuse.contents = UIImage(named: "art.scnassets/moon.jpg")
-//
-//        sphere.materials = [material]
-//
-//        let node = SCNNode()
-//
-//        node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
-//
-//        node.geometry = sphere
-//
-//        sceneView.scene.rootNode.addChildNode(node)
-        
         sceneView.autoenablesDefaultLighting = true
-        
-        
-        
-        // Show statistics such as fps and timing information
-        //sceneView.showsStatistics = true
-        
-        // Create a new scene
-        
-        
-
-
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-        
         configuration.planeDetection = .horizontal
 
-        // Run the view's session
         sceneView.session.run(configuration)
     }
     
@@ -94,52 +59,35 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                 
                 roll(dice: diceNode!)
                 
-                
             }
-            
         }
-        
     }
     
     @IBAction func removeAllDice(_ sender: UIBarButtonItem) {
         
         if !diceArray.isEmpty {
-            
             for dice in diceArray {
-                
                 dice.removeFromParentNode()
-                
             }
-            
         }
-        
     }
     
     
     func rollAll() {
-        
+
         if !diceArray.isEmpty {
-            
             for dice in diceArray {
-                
                 roll(dice: dice)
-                
             }
-            
         }
-        
     }
     
     @IBAction func rollAgain(_ sender: UIBarButtonItem) {
-        
         rollAll()
-        
     }
     
     override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
-        
         rollAll()
-        
     }
     
     func roll(dice: SCNNode) {
@@ -182,13 +130,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             planeNode.geometry = plane
             
             node.addChildNode(planeNode)
-            
+            DispatchQueue.main.async {
+                self.navigationController?.navigationBar.barTintColor = UIColor.green
+            }
             
         }
         
     }
-    
-    
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
@@ -196,35 +144,5 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
 
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
 }
